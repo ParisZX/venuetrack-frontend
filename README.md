eb1d9fbae956dcb4c4b84e2a5080532626616de0
ParisZX <parislagakis@gmail.com>
2015-08-05 21:34:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Venuetrack Frontend
+
+This is the frontend of an app currently in development that aims to be a mashup of Foursquare with custom ratings system, primarily for the city of Thessaloniki, Greece.
---
+++ app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'myApp.version',
+  'ngMap'
+]);
+
+myApp.controller('VenueListCtrl', function ($scope) {
+	$scope.venues = $data;
+});
+
