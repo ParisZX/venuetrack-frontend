506dc14b9a89e3ba3b6bc668663c9f975851169d
ParisZX <parislagakis@gmail.com>
2015-08-14 01:53:27 +0300

    Bootstrap theming, changes in the GUI, new search button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 (function() {
-	var myApp = angular.module('myApp',['venue']);
+	var myApp = angular.module('myApp',['ngMap','venue']);
 
 	myApp.controller('MainController', ['$scope', '$http' , 
 		function($scope, $http) {
 
-			$http.get('endpoints?what=venues').success(function(data) {    
+			$scope.venues = [];
+
+			var apiData = 'endpoints?what=venues';
+			var localData = 'data/data.json';
+
+			$http.get(localData).success(function(data) {    
 	    	
 	    	$scope.venues = data;
 	    
