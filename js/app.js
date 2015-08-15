95bc6445d509c2b6052587e92f12a6966daf7698
ParisZX <parislagakis@gmail.com>
2015-08-15 19:49:33 +0300

    trying a new approach on marker input..

--- js/app.js
+++ js/app.js
@@ -2,18 +2,38 @@
 	var myApp = angular.module('myApp',['ngMap','venue']);
 
 	myApp.controller('MainController', ['$scope', '$http' , 
+		
 		function($scope, $http) {
-
-			$scope.venues = [];
 
 			var apiData = 'endpoints?what=venues';
 			var localData = 'data/data.json';
-
+			var venues = [];
 			$http.get(localData).success(function(data) {    
-	    	
-	    	$scope.venues = data;
-	    
-	    });
+	    		venues = data;
+	    	});
+
+			var map;
+	  	
+		  	$scope.$on('mapInitialized', function(evt, evtMap) {
+		    	map = evtMap;
+		      	for (var i=0;i<venues.length;i++) {
+	      		  	var latLng = {lat: venues[i].lat, lng: venues[i].lng};
+		      		var marker = new google.maps.Marker({
+		      			title: venues[i].name,
+		      			position: latLng,
+		      			map: map,
+		      			icon: venues[i].categories[0].icon.prefix+"bg_32"+venues[i].categories[0].icon.suffix
+		      		});
+		      	}
+
+		      	var contentString ='<venue-details></venue-details>';
+
+
+				 
+			  	
+				
+			   	
+		    });
 
 	}]);
 
