197dd0aa90bbf758b3655140e939779cacc5e86a
Paris Lagakis <parislagakis@gmail.com>
2015-10-09 23:12:47 +0300

    nothing new, checkin something...

--- js/services.js
+++ js/services.js
@@ -3,6 +3,8 @@ var venuetrackServices = angular.module('venuetrackServices', ['ngResource']);
 venuetrackServices.factory('venuesAPI', ['$resource',
   function($resource){
     return $resource('data/:id.json', {}, {
-      query: {method:'GET', params:{id:'data'}, isArray:true}
+      query: {
+      	method:'GET', params:{id:'data'}, isArray:true
+      }
     });
   }]);
